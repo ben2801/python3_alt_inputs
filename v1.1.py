2deb154d1e400db8bdb7afde1978c20bca1ed0a2
Ben Myers <bmyers.freddy926@gmail.com>
2016-02-11 17:48:08 +1100

    Update and rename v1.0.py to v1.1.py
    New version, whitespace changes

--- v1.1.py
+++ v1.1.py
@@ -1,7 +1,7 @@
 '''
 Filename: python3_alt_inputs
 
-Version 1.0 - Initial release 11/02/16
+Version 1.1 - 11/02/16
 
 A Python3 module that contains input functions that automatically converts 
 inputs into specified data types. E.g. "intput" would take an input and convert it into an int.
